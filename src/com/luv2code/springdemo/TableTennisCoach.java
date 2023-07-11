defee8159a834270f6d7bbafb3e1b2179a66dfd0
gvkth <vokiencuong@gmail.com>
2023-07-11 23:51:35 +0700

    Practice #1

---
+++ src/com/luv2code/springdemo/TableTennisCoach.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo;
+
+public class TableTennisCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Practice serve 100 times";
+	}
+
+}
