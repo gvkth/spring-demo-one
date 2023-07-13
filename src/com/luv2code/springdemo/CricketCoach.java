6f52a39be9d9ef5478adfe8c209045281f6d92d4
gvkth <vokiencuong@gmail.com>
2023-07-13 22:38:54 +0700

    Setter injection: finish Lesson 37

---
+++ src/com/luv2code/springdemo/CricketCoach.java
@@ -0,0 +1,35 @@
+package com.luv2code.springdemo;
+
+public class CricketCoach implements Coach {
+	
+	private FortuneService fortuneService;
+
+	//create a no-arg constructor
+	public CricketCoach() {
+		System.out.println("CricketCoach: inside no-arg");
+	}
+	
+	//our setter method
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("CricketCoach: inside setter method");
+		this.fortuneService = fortuneService;
+	}
+
+
+
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Practice fast bowling for 15 minutes";
+	}
+
+	
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return fortuneService.getFortune();
+	}
+
+}
